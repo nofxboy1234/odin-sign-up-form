dbec9cc836e14dd0d7d5155b892c471c2b41c9bb
Dylan Palmboom <nofxboy1234@gmail.com>
2022-08-04 15:02:23 +0200

    Update final design

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('index.js');
+const userPassword = document.getElementById('userPassword');
+const userConfirmPassword = document.getElementById('userConfirmPassword');
+const passwordMessage = document.getElementById('passwordMessage');
+
+function checkPassword(event) {
+  if (userPassword.value !== userConfirmPassword.value) {
+    userPassword.classList.add('error');
+    userConfirmPassword.classList.add('error');
+    passwordMessage.classList.add('password-message');
+  } else {
+    userPassword.classList.remove('error');
+    userConfirmPassword.classList.remove('error');
+    passwordMessage.classList.remove('password-message');
+  }
+}
+
+userPassword.addEventListener('input', checkPassword);
+userConfirmPassword.addEventListener('input', checkPassword);
